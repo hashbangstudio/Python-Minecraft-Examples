8b1cec90cec3037d28c335a92425a9a29e6eab0e
HashBangStudio <hashbangstudio@users.noreply.github.com>
2014-05-11 05:37:41 +0000

    Convert tabs to spaces
    Tab conversion varies so to ensure consistency in indenting
    use all spaces so code displays correctly in all editors

--- 01-sendPositionToChat.py
+++ 01-sendPositionToChat.py
@@ -5,25 +5,25 @@ from mcpi.minecraft import *
 # Ensures that code in block is only run if script directly invoked
 if __name__ == "__main__":
 
-	"""
-	First thing you do is create a connection to minecraft
-	This is like dialling a phone.
-	It sets up a communication line between your script and the minecraft world
-	"""
-	
-	# Create a connection to Minecraft
-	# Any communication with the world must use this object
-	mc = Minecraft.create()
+    """
+    First thing you do is create a connection to minecraft
+    This is like dialling a phone.
+    It sets up a communication line between your script and the minecraft world
+    """
+
+    # Create a connection to Minecraft
+    # Any communication with the world must use this object
+    mc = Minecraft.create()
         
-        # Get the block that the player is currently in
-        playerPosition = mc.player.getTilePos()
-	
-        # create the output message as a string
-        message = "You are at "+ str((playerPosition.x, playerPosition.y, playerPosition.z))
+    # Get the block that the player is currently in
+    playerPosition = mc.player.getTilePos()
+    
+    # create the output message as a string
+    message = "You are at "+ str((playerPosition.x, playerPosition.y, playerPosition.z))
         
-        # print to the python interpreter standard output (terminal or IDLE probably)
-        print(message)
+    # print to the python interpreter standard output (terminal or IDLE probably)
+    print(message)
         
-        # send message to the minecraft chat
-        mc.postToChat(message) 
+    # send message to the minecraft chat
+    mc.postToChat(message) 
 
